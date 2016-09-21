e01c3c82e22ce906a878fe48b214d18cc66090d8
sanctus671 <taylor@taylorhamling.com>
2016-09-22 00:42:34 +1200

    .

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -115,6 +115,7 @@ angular.module('app.controllers', [])
 
     
     $scope.getAdvert = function(beacon){
+        $scope.modalOpen = true;
         $scope.advert = {};
         MainService.getAdvert(beacon).then(function(data){
             $scope.advert = data;
@@ -163,7 +164,7 @@ angular.module('app.controllers', [])
                 var x = result.x;
                 var y = result.y;
                 var z = result.z;
-                var isMoving = x > 1.2;
+                var isMoving = x > 1.2 || y > 1.2 || z > 1.2;
                 if ($scope.stage === 2 && $scope.acceleration.y > 7 && isMoving && Object.keys($rootScope.inRangeBeacons).length > 0 && !$scope.modalOpen && $state.current.name === "tab.drag"){
                     console.log("grabbed");
                     var beacon = {}; var proximity = false; 
