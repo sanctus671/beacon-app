d8702cf33f0a7f4c912005b9b260992fa1cbaefe
sanctus671 <taylor@taylorhamling.com>
2016-09-26 22:08:10 +1300

    .

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -112,7 +112,7 @@ angular.module('app.controllers', [])
     }
     
     $timeout(function(){
-        if (window.localStorage.external_load){
+        if (window.localStorage.external_load !== null){
             var url = window.localStorage.external_load;
             console.log(url);
             //grabad://2727/
